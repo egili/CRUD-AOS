829333951b450f856628884d4f2d6a796db75144
eliseu gili <gili.eliseup@gmail.com>
2022-05-14 17:34:16 -0300

    mudanças

--- CRUD-AOS/src/DBOs/Cidadao.java
+++ CRUD-AOS/src/DBOs/Cidadao.java
@@ -2,38 +2,39 @@ package DBOs;
 
 import project.Telefone;
 
-//TODO : esta classe esta incompleta !!
 public class Cidadao implements Cloneable{
 
-    private int CPF;
+    private long CPF;
     private String nome;
     private Telefone telefone;
     private int CEP;
     private char genero;
 
-    public Cidadao(int ra, String nome, int ddd, long numero) throws Exception {
-        setRa(ra);
+    public Cidadao(long cpf, String nome, int ddd, long numero, int cep, char genero) throws Exception {
+        setCPF(cpf);
         setNome(nome);
-        this.telefone = new Telefone(ddd, numero);
+        setTelefone(ddd, numero);
+        setCEP(cep);
+      //  this.genero = genero;
     }
-    public Cidadao(Cidadao a) throws Exception {
+    public Cidadao(Cidadao c) throws Exception {
 
-        if(a == null)
+        if(c == null)
             throw new Exception("aluno inexistente");
 
-        this.nome = a.nome;
-        this.CPF = a.CPF;
-        this.telefone = a.telefone;
+        setCPF(c.CPF);
+        setNome(c.nome);
+        this.telefone = new Telefone(c.telefone);
     }
 
-    public void setRa(int ra) throws Exception{
+    public void setCPF(long cpf) throws Exception{
 
-        if(ra < 0)
-            throw new Exception("o RA precisa ter o valor de um numero positivo");
+        if(cpf < 0)
+            throw new Exception("o CPF precisa ter o valor de um numero positivo");
 
-        CPF = ra;
+        CPF = cpf;
     }
-    public int getCPF(){
+    public long getCPF(){
         return CPF;
     }
 
@@ -47,16 +48,26 @@ public class Cidadao implements Cloneable{
         return nome;
     }
 
-    public void setTelefone(Telefone telefone) {
-        this.telefone = telefone;
+    public void setTelefone(int ddd, long numero) throws Exception {
+        this.telefone = new Telefone(ddd, numero);
     }
-    public Telefone getTelefone() {
-        return telefone;
+    public String getTelefone() {
+        return telefone.getTelefone();
+    }
+
+    public void setCEP(int cep) throws Exception{
+        if(cep < 0)
+            throw new Exception();
+
+        CEP = cep;
+    }
+    public int getCEP(){
+        return CEP;
     }
 
     @Override
     public String toString() {
-        return "Nome: " + getNome() + "\n RA: " + getCPF() + " \n telefone: " + getTelefone();
+        return "Nome: " + getNome() + "\n CPF: " + getCPF() + " \n telefone: " + getTelefone() + "\n CEP: " + getCEP();
     }
 
     @Override
@@ -75,6 +86,10 @@ public class Cidadao implements Cloneable{
             return false;
         if(this.telefone != cidadao.telefone)
             return false;
+        if(this.CEP != cidadao.CEP)
+            return false;
+        if(this.genero != cidadao.genero)
+            return false;
 
         return true;
     }
@@ -83,22 +98,24 @@ public class Cidadao implements Cloneable{
     public int hashCode() {
 
         int ret = 31;
-        ret = ret * 13 + Integer.valueOf(CPF).hashCode();
+        ret = ret * 13 + Long.valueOf(CPF).hashCode();
         ret = ret * 13 + String.valueOf(nome).hashCode();
         ret = ret * 13 + telefone.hashCode();
+        ret = ret * 13 + Integer.valueOf(CEP).hashCode();
+        ret = ret * 13 + Character.valueOf(genero);
 
         return ret < 0 ? -ret : ret;
     }
 
     public Object clone() {
 
-        Cidadao a = null;
+        Cidadao c = null;
 
         try{
-            a = new Cidadao(this);
+            c = new Cidadao(this);
         } catch (Exception e){
             e.printStackTrace();
         }
-        return a;
+        return c;
     }
 }
