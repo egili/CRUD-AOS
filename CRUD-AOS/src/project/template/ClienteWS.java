a5847d44f1cac358c8514a4e9f3839bdca0f38a3
eliseu gili <gili.eliseup@gmail.com>
2022-05-14 21:49:39 -0300

    novo package

--- CRUD-AOS/src/project/template/ClienteWS.java
+++ CRUD-AOS/src/project/template/ClienteWS.java
@@ -1,4 +1,4 @@
-package project;
+package project.template;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
--- CRUD-AOS/src/project/template/Logradouro.java
+++ CRUD-AOS/src/project/template/Logradouro.java
@@ -1,4 +1,4 @@
-package project;
+package project.template;
 
 public class Logradouro
 {
