f1869e8ce4ce49952dfa4d71a66d943f5d5d2645
eliseu gili <gili.eliseup@gmail.com>
2022-05-16 15:25:01 -0300

    primeira versao do CRUD

--- CRUD-AOS/src/DAOs/Delete.java
+++ CRUD-AOS/src/DAOs/Delete.java
@@ -1,4 +1,31 @@
 package DAOs;
 
+import project.template.bd.BDSQLServer;
+
+import java.sql.SQLException;
+
 public class Delete {
+
+    public static void delete(int cpf) throws Exception{
+
+        if(!CRUDHelper.cadastrado(cpf))
+            throw new Exception("nao cadastrado");
+
+        try{
+            String sql;
+
+            sql = "DELETE FROM cidadao " +
+                    "WHERE cid_cpf = ?";
+
+            BDSQLServer.COMANDO.prepareStatement(sql);
+
+            BDSQLServer.COMANDO.setInt(1, cpf);
+
+            BDSQLServer.COMANDO.executeUpdate();
+            BDSQLServer.COMANDO.commit();
+        } catch (SQLException err){
+            BDSQLServer.COMANDO.rollback();
+            throw new Exception("erro ao deletar o cidadao");
+        }
+    }
 }
--- CRUD-AOS/src/DAOs/Read.java
+++ CRUD-AOS/src/DAOs/Read.java
@@ -1,4 +1,56 @@
 package DAOs;
 
+import DBOs.Cidadao;
+import project.template.bd.BDSQLServer;
+import project.template.bd.core.MeuResultSet;
+
+import java.sql.SQLException;
+
 public class Read {
+
+    public static Cidadao read(int cpf) throws Exception{
+
+        Cidadao cidadao = null;
+
+        try {
+            String sql;
+
+            sql = "SELECT * " +
+                    "FROM cidadao" +
+                    "WHERE cid_cpf = ?";
+
+            BDSQLServer.COMANDO.prepareStatement(sql);
+            BDSQLServer.COMANDO.setInt(1, cpf);
+
+            MeuResultSet resultado = (MeuResultSet)BDSQLServer.COMANDO.executeQuery();
+
+            if(!resultado.first())
+                throw new Exception("sem dados correspondemtes ao parametro informado");
+
+            cidadao = New Cidadao(); // FIXME: dados do Cidadao
+        } catch (SQLException erro){
+            throw new Exception("erro ao procurar o cidadao");
+        } finally {
+            return cidadao;
+        }
+    }
+
+    public static MeuResultSet readAll() throws Exception{
+
+        MeuResultSet resultado = null;
+
+        try{
+            String sql;
+
+            sql = "SELECT * " + "FROM cidadao";
+
+            BDSQLServer.COMANDO.prepareStatement(sql);
+
+            resultado = (MeuResultSet)BDSQLServer.COMANDO.executeQuery();
+        } catch(SQLException err){
+            throw new Exception("erro ao ler os dados dos cidadaos");
+        } finally {
+            return resultado;
+        }
+    }
 }
